766f9de79c4920277fea09e64eae42ce1fb249f2
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-05 12:34:23 -0800

    Office Salary Processing System

---
+++ Q1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class Q1{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int basic=scan.nextInt();
+        int hra=scan.nextInt();
+        int bonus=scan.nextInt();
+        int tax=scan.nextInt();
+        int pf=scan.nextInt();
+        int value=basic+hra+bonus-tax-pf;
+        System.out.println(value);
+    }
+}
