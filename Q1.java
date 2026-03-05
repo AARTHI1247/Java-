d321c97b8da51fef537dbde7cf369009e1cdb0d6
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-05 12:35:20 -0800

    Office Salary Processing System

--- Q1.java
+++ Q1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
-public class Q1{
+public class Q1{ 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int basic=scan.nextInt();
