746a82e490b4ac95c1edcf147da8604fb53a29d4
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-05 21:09:17 -0800

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
-public class Q2{ 
+public class Q2{          
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int basecount=scan.nextInt();
