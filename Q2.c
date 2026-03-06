044cd0e83d94c95d5ae868b900c04310acdb53ed
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-05 20:49:24 -0800

    Mobile Data Plan Billing

---
+++ Q2.c
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class Q2{ 
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int basecount=scan.nextInt();
+        int extraGB=scan.nextInt();
+        int ratePerGB=scan.nextInt();
+        int tax=scan.nextInt();
+        int pd=extraGB*ratePerGB;
+        int sum=basecount+pd;
+        int tvalue=(sum*tax)/100;
+        int value=sum+tvalue;
+        System.out.println(value);
+    }
+}
