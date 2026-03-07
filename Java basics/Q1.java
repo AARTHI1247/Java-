34d2db690db06be8aa885fff07944b331732c8fa
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-07 09:01:50 -0800

    Update Q1.java

--- Java basics/Q1.java
+++ Java basics/Q1.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 public class Q1{ 
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);         
         int basic=scan.nextInt();
         int hra=scan.nextInt();
         int bonus=scan.nextInt();
