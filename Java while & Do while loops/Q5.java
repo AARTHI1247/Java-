ceee6795f926b2cbfc453742724d992ae25424da
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-13 02:58:43 -0700

    Reverse Number

--- Java while & Do while loops/Q5.java
+++ Java while & Do while loops/Q5.java
@@ -4,7 +4,7 @@ public class Q5{
     public static void main(String[]args){
         Scanner scan=new Scanner(System.in);
         int a=scan.nextInt();
-        int dig=0,rev=0,temp=a;;
+        int dig=0,rev=0,temp=a;
        while(a!=0){
            dig=a%10;
            rev=rev*10+dig;
