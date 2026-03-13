cbbb324d821a1bea25f9861b75d41e7bf6a3f4a0
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-13 02:03:05 -0700

    MenuDriven Calculator (DoWhile)

---
+++ Java while & Do while loops/Q8.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class Q8{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int dig=0,sum=0;
+        while(n>0){
+           dig=n%10;
+           sum=sum+dig;
+           n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
