7f959b3a31033b3bcf045e1d738a350ad485145a
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-13 02:05:16 -0700

    Digit Sum Calculator

--- Java while & Do while loops/Q8.java
+++ Java while & Do while loops/Q8.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
-public class Q8{
+public class Q8{ 
     public static void main(String[]args){
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
