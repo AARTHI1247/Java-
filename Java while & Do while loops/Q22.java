0872dba716e10d42b0bd54470bb5226d6a0e255e
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-12 22:11:43 -0700

    Sum of Even Numbers in Range

---
+++ Java while & Do while loops/Q22.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+public class Q24{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int sum=0;
+        int i=a;
+        while(i<=b){
+            if(i%2==0){
+               sum=sum+i;
+            }
+            i++;
+        }
+       System.out.println(sum);
+    }
+}
