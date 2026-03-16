954144e47b540b91f2ee678cd8721e2b4902774a
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-16 11:14:17 -0700

    Pattern Printer - Right Triangle

---
+++ Java Nested loop/Q3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner.*;
+public class Q3 {
+    public static void main(String[] args) {
+      Scanner scan=new Scanner(System.in);
+      int n=scan.nextInt();
+      String t=scan.next();
+      if(t.equalsIgnoreCase("star")){
+          for(int i=1;i<=n;i++){
+              for(int j=1;j<=i;j++){
+                  System.out.print("*");
+              }
+              System.out.println();
+          }
+      }
+      else if(t.equalsIgnoreCase("numbers")){
+        for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+             System.out.print(j);
+          }
+        System.out.println();  
+      }
+    }
+    }
+}
