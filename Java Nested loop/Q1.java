162a10891ac1142f502cdf3b106d2fd0133d5b20
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-15 23:51:08 -0700

    Multiplication Table Generator

--- Java Nested loop/Q1.java
+++ Java Nested loop/Q1.java
@@ -1,4 +1,20 @@
-hsjjkm
-fgrewrt5yuyuyt
-sdfkktrsdfg
-dfgyujuytre
+import java.util.*;
+import java.util.Scanner.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+      int r=scan.nextInt();
+      int c=scan.nextInt();
+      for(int i=1;i<=c;i++){
+          System.out.print(i+" ");
+      }
+      System.out.println();
+      for(int i=1;i<=r;i++){
+          System.out.print(i+" ");
+          for(int j=1;j<=c;j++){
+             System.out.print(i*j+" ");
+          }
+      System.out.println();
+      }
+    }
+}
