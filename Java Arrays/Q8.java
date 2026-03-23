7cd1a462d4112f892ab209e88e01e55980a4fbfc
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-23 04:40:22 -0700

    Update Q8.java

--- Java Arrays/Q8.java
+++ Java Arrays/Q8.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Q8{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);    
           int n=scan.nextInt();
           int sum=0,c=0;
           int arr[]=new int[n];
