e4f7c624ab713269f62974a132e505c42a35c7df
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-23 19:59:38 -0700

    Retail Sales Performance Dashboard

--- Java Arrays/Q9.java
+++ Java Arrays/Q9.java
@@ -1,1 +1,24 @@
-jjjui
+import java.util.*;
+public class Q9{     
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);    
+          int n=scan.nextInt();
+          double sum=0;
+          double arr[]=new double[n];
+          for(int i=0;i<n;i++){ 
+            arr[i]=scan.nextDouble();
+            sum=sum+arr[i];
+          }  
+          double max=arr[0],min=arr[0];
+          for(int i=0;i<n;i++){
+              if(max<arr[i]){
+                  max=arr[i];
+              }
+              if(min>arr[i]){
+                 min=arr[i]; 
+              }
+          }
+          double av=sum/n;
+          System.out.print("Total Stores:"+n+"\n"+"Highest Sales: $"+String.format("%.2f",max)+"\n"+"Lowest Sales: $"+String.format("%.2f",min)+"\n"+"Total Sales: $"+String.format("%.2f",sum)+"\n"+"Average Sales: $"+String.format("%.2f",av));
+    }
+}
