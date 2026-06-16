fd61029254b1f4eb0cd4423da36c0e64036ef5f1
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-06-16 22:50:04 +0530

    Add random class to handle ArrayList operations
    Implement a class to read integers into an ArrayList and print the first and last elements.

---
+++ Java Collections/ArrayList/Q3.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class random {
+    static void main() {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(scan.nextInt());
+        }
+        System.out.println(arr.get(0));
+        System.out.print(arr.get(n-1));
+    }
+}
