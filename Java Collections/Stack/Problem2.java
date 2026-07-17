238947d354a5bd2cb15790b572e5deacd43e1a6e
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-07-17 13:00:44 +0530

    Add balanced parentheses checker using Stack
    Implement a program to check balanced parentheses using a stack.

---
+++ Java Collections/Stack/Problem2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class ran1 {
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt(),c=0;
+        String str=scan.next();
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<n;i++){
+           if(str.charAt(i)=='(' ||str.charAt(i)=='{'||str.charAt(i)=='['){
+               s.push(str.charAt(i));
+           }
+           else{
+               if(str.charAt(i)==')'){
+                   char ch=s.pop();
+                   if(ch=='('){
+                       c++;
+                   }
+               }
+               if(str.charAt(i)=='}'){
+                   char ch=s.pop();
+                   if(ch=='{'){
+                       c++;
+                   }
+               }
+               if(str.charAt(i)==']'){
+                   char ch=s.pop();
+                   if(ch=='['){
+                       c++;
+                   }
+               }
+           }
+        }
+        if(c==n/2){
+            System.out.println("true");
+            return;
+        }
+        System.out.println("false");
+    }
+}
