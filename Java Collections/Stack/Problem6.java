34e84ed9ab133900b4e2d8b8094afb4285c17dcb
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-07-17 14:38:06 +0530

    Add decimal to binary conversion using Stack
    Implement a program to convert a decimal number to binary using a stack.

---
+++ Java Collections/Stack/Problem6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Problem5 {
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        Stack<Integer> s = new Stack<>();
+        while(n>0){
+            int rem=n%2;
+            s.push(rem);
+            n/=2;
+        }
+        while(!s.empty()){
+            System.out.print(s.pop());
+        }
+    }
+}
