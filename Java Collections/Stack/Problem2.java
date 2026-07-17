a441c20e1073366fdd5fa1f4d6935e748fb462ce
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-07-17 13:01:45 +0530

    Balanced Parentheses

--- Java Collections/Stack/Problem2.java
+++ Java Collections/Stack/Problem2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ran1 {
+public class Problem2 {
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
         int n=scan.nextInt(),c=0;
